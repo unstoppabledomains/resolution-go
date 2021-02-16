c7047f27417a3a72f07c2df824039b720a3e7053
Kirill Beresnev <derainberk@gmail.com>
2021-02-17 02:29:17 +0300

    Finish Zilliqa query

--- zns.go
+++ zns.go
@@ -1,13 +1,15 @@
 package resolution
 
 import (
-	"fmt"
+	"encoding/json"
 	"github.com/Zilliqa/gozilliqa-sdk/provider"
+	"strings"
 )
 
 const znsDefaultProvider = "https://api.zilliqa.com"
 const znsMainnetRegistry = "9611c53BE6d1b32058b2747bdeCECed7e1216793"
 const znsContractField = "records"
+const znsZeroAddress = "0x0000000000000000000000000000000000000000"
 
 type ZnsProvider interface {
 	GetSmartContractSubState(contractAddress string, params ...interface{}) (string, error)
@@ -15,6 +17,24 @@ type ZnsProvider interface {
 
 type Zns struct {
 	Provider ZnsProvider
+}
+
+type registrySubState struct {
+	Result map[string]map[string]struct {
+		Arguments   []string
+		Argtypes    []string
+		Constructor string
+	}
+}
+
+type resolverSubState struct {
+	Result map[string]map[string]string
+}
+
+type znsDomainState struct {
+	Resolver string
+	Owner    string
+	Records  map[string]string
 }
 
 func NewZns(provider ZnsProvider) *Zns {
@@ -25,16 +45,43 @@ func NewZnsWithDefaultProvider() *Zns {
 	return &Zns{Provider: provider.NewProvider(znsDefaultProvider)}
 }
 
-func (z *Zns) State(domainName string, keys []string) {
+func (z *Zns) State(domainName string, keys []string) (*znsDomainState, error) {
 	namehash, err := ZnsNameHash(domainName)
 	if err != nil {
-		return
+		return nil, err
 	}
-	state, err := z.Provider.GetSmartContractSubState(znsMainnetRegistry, znsContractField, []string{namehash})
+	response, err := z.Provider.GetSmartContractSubState(znsMainnetRegistry, znsContractField, []string{namehash})
 	if err != nil {
-		// todo continue with resolver query
-		fmt.Println(err)
-	} else {
-		fmt.Println(state)
+		return nil, err
 	}
+
+	var registryState registrySubState
+	err = json.Unmarshal([]byte(response), &registryState)
+	if err != nil {
+		return nil, err
+	}
+	registryValues := registryState.Result[znsContractField][namehash].Arguments
+	if len(registryValues) == 0 {
+		return nil, &DomainNotRegistered{DomainName: domainName}
+	}
+	owner, resolver := registryValues[0], registryValues[1]
+	if owner == znsZeroAddress {
+		return nil, &DomainNotRegistered{DomainName: domainName}
+	}
+	if resolver == znsZeroAddress {
+		return nil, &DomainNotConfigured{DomainName: domainName}
+	}
+
+	response, err = z.Provider.GetSmartContractSubState(strings.TrimPrefix(resolver, "0x"), znsContractField, keys)
+	if err != nil {
+		return nil, err
+	}
+	var resolverState resolverSubState
+	err = json.Unmarshal([]byte(response), &resolverState)
+	if err != nil {
+		return nil, err
+	}
+	records := resolverState.Result[znsContractField]
+
+	return &znsDomainState{Owner: owner, Resolver: resolver, Records: records}, nil
 }
--- zns_test.go
+++ zns_test.go
@@ -21,8 +21,26 @@ func TestNewZnsWithDefaultProvider(t *testing.T) {
 	assert.IsType(t, &Zns{Provider: nil}, zns)
 }
 
+// todo check edge cases
 func TestZnsState(t *testing.T) {
 	t.Parallel()
-	zns.State("brad.zil", []string{})
-	// todo finish the test
+	expectedRecord := map[string]string{
+		"ipfs.html.value":            "QmVaAtQbi3EtsfpKoLzALm6vXphdi2KjMgxEDKeGg6wHuK",
+		"crypto.BCH.address":         "qrq4sk49ayvepqz7j7ep8x4km2qp8lauvcnzhveyu6",
+		"crypto.BTC.address":         "1EVt92qQnaLDcmVFtHivRJaunG2mf2C3mB",
+		"crypto.ETH.address":         "0x45b31e01AA6f42F0549aD482BE81635ED3149abb",
+		"crypto.LTC.address":         "LetmswTW3b7dgJ46mXuiXMUY17XbK29UmL",
+		"crypto.XMR.address":         "447d7TVFkoQ57k3jm3wGKoEAkfEym59mK96Xw5yWamDNFGaLKW5wL2qK5RMTDKGSvYfQYVN7dLSrLdkwtKH3hwbSCQCu26d",
+		"crypto.ZEC.address":         "t1h7ttmQvWCSH1wfrcmvT4mZJfGw2DgCSqV",
+		"crypto.ZIL.address":         "zil1yu5u4hegy9v3xgluweg4en54zm8f8auwxu0xxj",
+		"crypto.DASH.address":        "XnixreEBqFuSLnDSLNbfqMH1GsZk7cgW4j",
+		"ipfs.redirect_domain.value": "www.unstoppabledomains.com",
+	}
+	expectedOwner := "0x2d418942dce1afa02d0733a2000c71b371a6ac07"
+	expectedResolver := "0xdac22230adfe4601f00631eae92df6d77f054891"
+	state, err := zns.State("brad.zil", []string{})
+	assert.Nil(t, err)
+	assert.Equal(t, expectedRecord, state.Records)
+	assert.Equal(t, expectedOwner, state.Owner)
+	assert.Equal(t, expectedResolver, state.Resolver)
 }
